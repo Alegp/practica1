499243950dbbadccf5f0c43b748fbfd59e3e57f0
Alegp <alegp6@gmail.com>
2024-04-09 16:24:48 +0200

    Cambio codigo completo
    He cambiado todo el codigo para estructurarlo en metodos cada uno con su funcion especifica

--- M05UF2Practica1/src/practica1/Practica1.java
+++ M05UF2Practica1/src/practica1/Practica1.java
@@ -1,11 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-//https://docs.github.com/en/desktop/making-changes-in-a-branch/committing-and-reviewing-changes-to-your-project-in-github-desktop
-//https://stackoverflow.com/questions/12951588/in-github-can-i-clone-merge-branch-online
 package practica1;
 
 import java.util.Scanner;
@@ -16,68 +8,45 @@ import java.util.Scanner;
  */
 public class Practica1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        double res = 0;
-        String operacion;
-        boolean comprobar = false;
+        Scanner scanner = new Scanner(System.in);
 
         do {
+            double n1 = obtenerNumero(scanner, "Introduce el primer número:");
+            String operacion = obtenerOperacion(scanner);
+            double n2 = obtenerNumero(scanner, "Introduce el segundo número:");
 
-            String numero1;
-            do {
-                System.out.println("\n Introdueix el primer numero. ");
-                numero1 = sc.nextLine();
-            } while (!numero1.matches("[+-]?[\\d]*[.]?[\\d]+"));
-            double nume1 = Double.parseDouble(numero1);
-            double n1 = new Double(numero1);
+            double resultado = calcularOperacion(n1, operacion, n2);
 
-            do {
-                System.out.println("\n Operació? (Indica el signe)");
-                System.out.println("+ = sumar \n - = restar \n"
-                        + " x = multiplicar \n / = dividir \n * = elevar primer num al segon num."
-                        + "\n % = residu");
-                operacion = sc.nextLine();
-                if (operacion.equals("+") || operacion.equals("-") || operacion.equals("x")
-                        || operacion.equals("X") || operacion.equals("/") || operacion.equals("%")
-                        || operacion.equals("*")) {
-                    comprobar = true;
-                } else {
-                    comprobar = false;
-                }
-            } while (comprobar != true);
+            System.out.println("(" + n1 + ") " + operacion + " (" + n2 + ")" + " = " + resultado);
 
-            String numero2;
-            do {
-                System.out.println("\n Introdueix el segon numero.");
-                numero2 = sc.nextLine();
-            } while (!numero2.matches("[+-]?[\\d]*[.]?[\\d]+"));
-            double nume2 = Double.parseDouble(numero2);
-            double n2 = new Double(numero2);
-
-            System.out.println("(" + numero1 + ") " + operacion + " (" + numero2 + ")" + " = " + res);
-            System.out.println("\n Vols continuar operant? \n");
-            System.out.println(" [s/n]");
-            do {
-                comprobar = true;
-                operacion = sc.nextLine();
-
-                switch (operacion) {
-                    case "s":
-                    case "S":
-                    case "n":
-                    case "N":
-                        break;
-                    default:
-                        System.err.println("\n Error, posa un valor vàlid. \n");
-                        comprobar = false;
-                }
-            } while (comprobar != true);
-        } while (operacion.equals("s") || operacion.equals("S"));
+            System.out.println("\n¿Quieres continuar operando? [s/n]");
+        } while (scanner.nextLine().equalsIgnoreCase("s"));
     }
+
+    private static double obtenerNumero(Scanner scanner, String mensaje) {
+        double numero;
+        do {
+            System.out.print(mensaje + " ");
+            while (!scanner.hasNextDouble()) {
+                System.out.println("Por favor, introduce un número válido.");
+                scanner.next();
+            }
+            numero = scanner.nextDouble();
+        } while (numero == 0);
+        return numero;
+    }
+
+    private static String obtenerOperacion(Scanner scanner) {
+        String operacion;
+        do {
+            System.out.println("\n¿Qué operación deseas realizar? (Indica el símbolo)\n");
+            System.out.println("+ = sumar \n- = restar \n* = multiplicar \n/ = dividir \n^ = elevar primer número al segundo número.\n% = residuo");
+            operacion = scanner.nextLine();
+        } while (!operacion.matches("[+\\-*/^%]"));
+        return operacion;
+    }
+
     private static double calcularOperacion(double n1, String operacion, double n2) {
         switch (operacion) {
             case "+":
